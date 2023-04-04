8ede91ba5037e9e31a09432737ad79156a7c7647
mingulee <100409006+mingulee-devel@users.noreply.github.com>
2023-04-05 04:09:57 +0900

    Update 구슬을 나누는 경우의 수.java

--- 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.java
+++ 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.java
@@ -5,23 +5,23 @@ class Solution {
         int answer = 0;
         double un1 = 1;
         double un2 = 1;
-        double dn = 1; //곱한 값이 오버플로우 날 수 있음
+        double dn = 1; //1.곱한 값이 오버플로우 날 수 있음
         
-        for(int i = balls - share + 1; i <= balls  ; i++){
-            un1 = un1 * i;
-        }
+        // for(int i = balls - share + 1; i <= balls  ; i++){
+        //     un1 = un1 * i;
+        // }
         
         for(int i = balls; i >= balls - share + 1 ; i--){
             un2 = un2 * i;
         }
         
-        System.out.println(un1);
-        System.out.println(un2);
-        
         for(int i = share; i > 0 ; i--){
             dn = dn * i;
-        }
-        answer = (int)Math.round(un1/dn);
+        } 
+        
+        answer = (int)Math.round(un2/dn); //2.부동소수점 오류 방지를 위해 반올림 또는 역순 for문은 오류 안남
+        // answer = (int)(un1/dn); //케이스3 실패 뜨는 코드
+        // answer = (int)(un2/dn); //역순 for문
         
         return answer;
     }

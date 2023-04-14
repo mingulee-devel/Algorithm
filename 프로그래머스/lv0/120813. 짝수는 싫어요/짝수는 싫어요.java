d04a01df0faa8881f7698db839b5992a6a3fb1fc
mingulee <100409006+mingulee-devel@users.noreply.github.com>
2023-04-14 23:47:43 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -1,14 +1,35 @@
-import java.util.*;
+// import java.util.*;
+
+// class Solution {
+//     public int[] solution(int n) {
+//         int[] answer = {};
+//         List<Integer> list = new ArrayList<>();
+//         for(int i = 1; i <= n; i++){
+//             if(i%2 != 0) list.add(i);
+//         }
+        
+//         answer = list.stream().mapToInt(i -> i).toArray();
+//         return answer;
+//     }
+// }
+
+// 참고할만한 다른 사람 풀이
 
 class Solution {
     public int[] solution(int n) {
-        int[] answer = {};
-        List<Integer> list = new ArrayList<>();
-        for(int i = 1; i <= n; i++){
-            if(i%2 != 0) list.add(i);
+        int cnt = 0;
+
+        if(n % 2 == 0) cnt = n/2;
+        else cnt = n/2 + 1;
+
+        int[] result = new int[cnt];
+        int num = 1;
+
+        for(int i=0; i<result.length; i++){
+            result[i] = num;
+            num = num + 2;
         }
-        
-        answer = list.stream().mapToInt(Integer::intValue).toArray();
-        return answer;
+
+        return result;
     }
 }

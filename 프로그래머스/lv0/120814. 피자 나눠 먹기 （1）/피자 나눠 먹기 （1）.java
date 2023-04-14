45139721b06c0746bb9e032d22e1ae37417d1553
mingulee <100409006+mingulee-devel@users.noreply.github.com>
2023-04-15 01:41:42 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.02 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -1,8 +1,11 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        if(n % 7 == 0) answer = n/7;
-        else answer = (n/7) + 1;
-        return answer;
+        // int answer = 0;
+        // if(n % 7 == 0) answer = n/7;
+        // else answer = (n/7) + 1;
+        // return answer;
+        
+        //다른 사람 풀이 참고
+        return ( n + 6 ) / 7;
     }
 }

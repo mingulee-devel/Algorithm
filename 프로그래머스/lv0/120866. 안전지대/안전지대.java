d080f262923c1830579eb146ae07a15eba96afc1
mingulee <100409006+mingulee-devel@users.noreply.github.com>
2023-04-30 14:14:07 +0900

    안전지대 2번째 풀이
    테스트 결과
    테스트 1 〉	통과 (0.02ms, 72.6MB)
    테스트 2 〉	통과 (0.03ms, 79MB)
    테스트 3 〉	통과 (0.02ms, 70.7MB)
    테스트 4 〉	통과 (0.04ms, 79.9MB)
    테스트 5 〉	통과 (0.02ms, 72.9MB)
    테스트 6 〉	통과 (0.05ms, 73.6MB)
    테스트 7 〉	통과 (0.03ms, 71MB)
    테스트 8 〉	통과 (0.02ms, 79MB)
    테스트 9 〉	통과 (0.02ms, 73.8MB)
    테스트 10 〉	통과 (0.04ms, 77.3MB)
    테스트 11 〉	통과 (0.02ms, 74.3MB)
    테스트 12 〉	통과 (0.03ms, 74.2MB)
    테스트 13 〉	통과 (0.02ms, 62.2MB)
    테스트 14 〉	통과 (0.02ms, 73MB)

--- 프로그래머스/lv0/120866. 안전지대/안전지대.java
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-    
 class Solution {
     public int solution(int[][] board) {
         int answer = 0;
@@ -7,33 +5,31 @@ class Solution {
             for(int j = 0 ; j < board[i].length ; j++){
                 if(board[i][j] == 1 || board[i][j] == 2 ) continue;
                 if(j-1 >= 0 && board[i][j-1] == 1) {
-                    board[i][j] = 2; continue;
+                    continue;
                 } else if(j+1 < board[i].length && board[i][j+1] == 1) {
-                    board[i][j] = 2; continue;
+                    continue;
                 }
                 if(i+1 < board.length){
                     if(board[i+1][j] == 1) {
-                        board[i][j] = 2; continue;
+                        continue;
                     } else if(j-1 >= 0 && board[i+1][j-1] == 1) {
-                        board[i][j] = 2; continue;
+                        continue;
                     } else if(j+1 < board[i].length && board[i+1][j+1] == 1) {
-                        board[i][j] = 2; continue;
+                        continue;
                     }
                 }
                 if(i-1 > 0){
                     if(board[i-1][j] == 1) {
-                        board[i][j] = 2; continue;
+                        continue;
                     } else if(j-1 >= 0 && board[i-1][j-1] == 1) {
-                        board[i][j] = 2; continue;
+                        continue;
                     } else if(j+1 < board[i].length && board[i-1][j+1] == 1) {
-                        board[i][j] = 2; continue;
+                        continue;
                     }
                 }
+                answer += 1;
             }
         }
-        String str = Arrays.deepToString(board);
-        System.out.println(str);
-        answer = str.length() - str.replace("0", "").length();
         return answer;
     }
 }

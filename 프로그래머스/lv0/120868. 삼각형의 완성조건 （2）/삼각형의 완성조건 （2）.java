b9c67480d0ae5ec5cc12a247b5a647224e9b0de0
mingulee <100409006+mingulee-devel@users.noreply.github.com>
2023-05-17 22:32:33 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.02 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
+++ 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
@@ -4,8 +4,7 @@ import java.util.Set;
 class Solution {
     public int solution(int[] sides) {
         int answer = 0;
-        
-        Set<Integer> test = new HashSet<>();
+
         //큰 값을 [0], 작은 값을 [1]에 담아줌.
         if(sides[0] < sides[1]) {
             int temp = sides[0];
@@ -16,13 +15,11 @@ class Solution {
         //둘 중 하나가 긴 변인 경우 (1 <= x < max)
         //나머지 한 변이 가장 긴 변인 경우 (max <= x < max+min)
         for(int i = 1; i < sides[0]+sides[1] ; i++){
-            if(i+sides[1] <= sides[0]) continue;
-            if(sides[1]+sides[0] <= i) continue;
-            if(sides[0]+i <= sides[1]) continue;
-            test.add(i);
+            //i가 가장 긴 변일 때,
+            if(i >= sides[0] && sides[0]+sides[1] > i) answer++;
+            else if(i <= sides[0] && i+sides[1] > sides[0]) answer++;
         }
-        
-        answer = test.size();
+
         return answer;
     }
 }

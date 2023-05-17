666c88c1416b8b1e542daf0cfb2fde9caaa76f81
mingulee <100409006+mingulee-devel@users.noreply.github.com>
2023-05-17 23:54:59 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.24 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.java
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.java
@@ -2,16 +2,11 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         for(int i = 0 ; i < n ; i++){
-            answer = checkFunc(answer);
+            answer++;
+            if((answer >= 3 && answer % 3 == 0) || Integer.toString(answer).contains("3")) {
+                i--;
+            }
         }
         return answer;
-    }
-    
-    public int checkFunc(int a){
-        a++;
-        if((a >= 3 && a % 3 == 0) || Integer.toString(a).contains("3")) {
-            return checkFunc(a);
-        }
-        return a;
     }
 }

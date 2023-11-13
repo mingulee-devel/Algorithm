73431216a6bc795ac728b38c67a3569764713768
mingulee <100409006+mingulee-devel@users.noreply.github.com>
2023-11-14 00:38:25 +0900

    [level unrated] Title: 코드 처리하기, Time: 0.02 ms, Memory: 71 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String code) {
+        String answer = "";
+        int mode = 0;
+        for(int i = 0 ; i < code.length() ; i++){
+            if(code.charAt(i) == '1') {
+                mode = (mode == 0 ? 1 : 0);
+                continue;
+            }
+            
+            if((mode == 0) == (i % 2 == 0) ) answer += code.charAt(i);
+        }
+        if(answer.length() == 0 ) return "EMPTY";
+        return answer;
+    }
+}

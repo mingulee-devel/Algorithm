5c099a3ea43891cf127705922aa1888e2d32df69
mingulee <100409006+mingulee-devel@users.noreply.github.com>
2023-12-23 00:37:34 +0900

    [level unrated] Title: 문자열 겹쳐쓰기, Time: 1.14 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        String answer = my_string.substring(0, s) + overwrite_string;
+        if(my_string.length() > s+overwrite_string.length()) answer += my_string.substring(s+overwrite_string.length(), my_string.length());
+        return answer;
+    }
+}

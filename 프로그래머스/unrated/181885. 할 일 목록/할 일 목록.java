1c5d7b4db73d562ee12100be40b437964f243437
mingulee <100409006+mingulee-devel@users.noreply.github.com>
2023-12-23 01:37:39 +0900

    [level unrated] Title: 할 일 목록, Time: 8.02 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.java
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.java
@@ -3,10 +3,17 @@ import java.util.ArrayList;
 
 class Solution {
     public String[] solution(String[] todo_list, boolean[] finished) {
-        List<String> answer = new ArrayList<>();
-        for(int i = 0; i < todo_list.length ; i++){
-            if(finished[i] == false) answer.add(todo_list[i]);
+        // List<String> answer = new ArrayList<>();
+        // for(int i = 0; i < todo_list.length ; i++){
+        //     if(finished[i] == false) answer.add(todo_list[i]);
+        // }
+        // return answer.toArray(new String[answer.size()]);
+        
+        String str = "";
+        for(int i=0; i<finished.length; i++){
+            str = finished[i]==false ? str+todo_list[i]+"," : str;
         }
-        return answer.toArray(new String[answer.size()]);
+
+        return str.split(",");
     }
 }
